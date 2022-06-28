5fbd26599b2743111da4513e3684410b18290f86
clgbatista <clgbatista@pm.me>
2022-06-28 11:36:23 +0300

    handling file/directory errors

--- python_scritps/contact_to_csv.py
+++ python_scritps/contact_to_csv.py
@@ -9,18 +9,26 @@ data = pd.DataFrame()
 
 try:
     for file_name in listdir('data/'):
+        
+        if file_name.endswith('.txt'):
+            df = gmat.contact_to_csv(file_name)
+            # print(len(df))
+            data = pd.concat([data,df],ignore_index=True)
 
-        df = gmat.contact_to_csv(file_name)
-        # print(len(df))
-        data = pd.concat([data,df],ignore_index=True)
+            print('File {} appended'.format(file_name))
 
-        print('File {} appended'.format(file_name))
+            file_to_save = 'contact.csv'
+            err = data.to_csv(file_to_save,index=False)
+        else:
+            break
+
+    if not err:
+        print('\nSaved to file {}'.format(file_to_save))
+    else:
+        print('\nUnable to save file')
+
 except:
-    print('No file or directory')
+    print('No .txt file or directory')
 
-file_to_save = 'contact.csv'
-err = data.to_csv(file_to_save,index=False)
 
-if not err:
-    print('\nSaved to file {}'.format(file_to_save))
 
