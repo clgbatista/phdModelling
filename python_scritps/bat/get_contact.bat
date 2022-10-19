9afa2d638a1dfd294bba2957f7bbb22c3aecf391
clgbatista <clgbatista@pm.me>
2022-10-19 10:27:59 +0300

    playing around with batch file to copy results, different configs constellation

---
+++ python_scritps/bat/get_contact.bat
@@ -0,0 +1,5 @@
+@echo off
+
+xcopy /s /y C:\Users\carlos.batista\Documents\.app\GMAT\output\*Contact.txt C:\Users\carlos.batista\Documents\.coding\plantuml\phdModelling\python_scritps\data
+
+xcopy /s /y C:\Users\carlos.batista\Documents\.coding\gmat\phdSim\*.script C:\Users\carlos.batista\Documents\.coding\plantuml\phdModelling\gmat\phdSim
--- python_scritps/libs/plot_map.py
+++ python_scritps/libs/plot_map.py
@@ -1,4 +1,10 @@
+import matplotlib.pyplot as plt
 import pandas as pd
+import plotly as plotly
+import plotly.express as px
+import json
+
+
 
 data = pd.read_csv('geojson/brazil_geo.json')
 
