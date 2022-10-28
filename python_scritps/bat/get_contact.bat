b3e94bc913f899c61c2a52b811e3a0520092e32f
clgbatista <clgbatista@pm.me>
2022-10-28 17:14:18 +0300

    refactoring

--- python_scritps/bat/get_contact.bat
+++ python_scritps/bat/get_contact.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo =========================================================
-echo           Copying files from GMAT
+echo           Copying files from GMAT - %~nx0
 echo:
 
 xcopy /s /y C:\Users\carlos.batista\Documents\.app\GMAT\output\*Contact.txt C:\Users\carlos.batista\Documents\.coding\plantuml\phdModelling\python_scritps\data
