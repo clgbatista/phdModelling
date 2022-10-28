d12447143f3a5dc2b677edd2a9b519742711f62d
clgbatista <clgbatista@pm.me>
2022-10-28 15:34:00 +0300

    adding func to the batch file

--- python_scritps/bat/get_contact.bat
+++ python_scritps/bat/get_contact.bat
@@ -1,5 +1,13 @@
 @echo off
 
+echo =========================================================
+echo           Copying files from GMAT
+echo:
+
 xcopy /s /y C:\Users\carlos.batista\Documents\.app\GMAT\output\*Contact.txt C:\Users\carlos.batista\Documents\.coding\plantuml\phdModelling\python_scritps\data
 
 xcopy /s /y C:\Users\carlos.batista\Documents\.coding\gmat\phdSim\*.script C:\Users\carlos.batista\Documents\.coding\plantuml\phdModelling\gmat\phdSim
+
+py ..\contact_to_csv.py -i CONASAT1Contact.txt -o contactCON1
+
+py ..\gen_contact_table.py -i contactCON1.csv -o coverageCON1
