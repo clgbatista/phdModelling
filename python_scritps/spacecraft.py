3ccc768d0a56970eae9609f4e1d817957443e08d
clgbatista <clgbatista@pm.me>
2023-02-22 17:10:13 +0200

    spacecraft include file generation from a json file, one for each satellite

--- python_scritps/spacecraft.py
+++ python_scritps/spacecraft.py
@@ -1,9 +1,30 @@
 
 import libs.from_gmat as gmat
+import json as json
+
+f = open("./python_scritps/data/scd/scd1.json")
+
+data = json.load(f)
+print("Target "+data["target"])
+f.close
 
 f = open("./gmat/phdSim/spacecraft.txt", "w")
-f.write("GMAT SCD1.SMA = 7121.000000000004;")
+f.write("Create Spacecraft "+data["target"]+";\n")
 f.close()
+
+path_to_file = "./gmat/phdSim/spacecraft.txt"
+
+for i in data["parameters"] :
+    f = open(path_to_file,"a")
+    if i != "keplerianElements":
+        info = i+" = "+str(data["parameters"][i])
+        # f.write("GMAT "+data["target"]+"."+info+";\n")
+    else :
+        for kepler in data["parameters"]["keplerianElements"] :
+            info = kepler+" = "+str(data["parameters"]["keplerianElements"][kepler])
+            # f.write("GMAT "+data["target"]+"."+info+";\n")
+    f.write("GMAT "+data["target"]+"."+info+";\n")
+    f.close()
 
 f = open("./gmat/phdSim/spacecraft.txt","r")
 print(f.read())
